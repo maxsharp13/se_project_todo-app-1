3860121ef8f6d32d326fbf6d16bb1a233d46e14c
Max Sharpnack <Maxsharpnack@gmail.com>
2026-01-12 13:37:12 -0600

    todo update

--- components/todo.js
+++ components/todo.js
@@ -1,59 +1,58 @@
 export default class Todo {
-    constructor(data, selector) {
-      this._id = data.id;
-      this._name = data.name;
-      this._completed = data.completed;
-      this._date = data.date;
-      this._selector = selector;
+  constructor(data, selector) {
+    this._id = data.id;
+    this._name = data.name;
+    this._completed = data.completed;
+    this._date = data.date;
+    this._selector = selector;
+  }
+
+  _getTemplate() {
+    return document
+      .querySelector(this._selector)
+      .content
+      .querySelector(".Todo")
+      .cloneNode(true);
+  }
+
+  _generateNameEl() {
+    this._nameEl = this._element.querySelector(".Todo__name");
+    this._nameEl.textContent = this._name;
+  }
+
+  _generateDateEl() {
+    this._dateEl = this._element.querySelector(".Todo__date");
+    const dueDate = new Date(this._date);
+
+    if (!isNaN(dueDate)) {
+      this._dateEl.textContent = `Due: ${dueDate.toLocaleString("en-US", {
+        year: "numeric",
+        month: "short",
+        day: "numeric",
+      })}`;
     }
-  
-    _getTemplate() {
-      return document
-        .querySelector(this._selector)
-        .content
-        .querySelector(".Todo")
-        .cloneNode(true);
-    }
-  
-    _generateNameEl() {
-      this._nameEl = this._element.querySelector(".Todo__name");
-      this._nameEl.textContent = this._name;
-    }
-  
-    _generateDateEl() {
-      this._dateEl = this._element.querySelector(".Todo__date");
-      const dueDate = new Date(this._date);
-  
-      if (!isNaN(dueDate)) {
-        this._dateEl.textContent = `Due: ${dueDate.toLocaleString("en-US", {
-          year: "numeric",
-          month: "short",
-          day: "numeric",
-        })}`;
-      }
-    }
-  
-    _generateCheckboxEl() {
-      this._checkboxEl = this._element.querySelector(".Todo__completed");
-      this._checkboxLabel = this._element.querySelector(".Todo__label");
-  
-      this._checkboxEl.checked = this._completed;
-      this._checkboxEl.id = `todo-${this._id}`;
-      this._checkboxLabel.setAttribute("for", `todo-${this._id}`);
-    }
-  
-    _setEventListeners() {
-      this._deleteBtnEl.addEventListener("click", () => {
-        this._element.remove();
-      });
-  
-      this._checkboxEl.addEventListener("change", () => {
-        this._completed = !this._completed;
-      });
-    }
-  
-    getView() {
-      this._element = this._getTemplate();
+  }
+
+  _generateCheckboxEl() {
+    this._checkboxEl = this._element.querySelector(".Todo__completed");
+    this._checkboxLabel = this._element.querySelector(".Todo__label");
+
+    this._checkboxEl.checked = this._completed;
+    this._checkboxEl.id = `todo-${this._id}`;
+    this._checkboxLabel.setAttribute("for", `todo-${this._id}`);
+  }
+
+  _setEventListeners() {
+    this._deleteBtnEl.addEventListener("click", () => {
+      this._element.remove();
+    });
+
+    this._checkboxEl.addEventListener("change", () => {
+      this._completed = !this._completed;
+    });
+  }
+  getView () {
+  this._element = this._getTemplate();
       this._deleteBtnEl = this._element.querySelector(".Todo__delete-btn");
   
       this._generateNameEl();
@@ -64,4 +63,3 @@ export default class Todo {
       return this._element;
     }
   }
-  
--- pages/index.js
+++ pages/index.js
@@ -2,56 +2,56 @@ import { initialTodos, validationConfig } from "../utils/constants.js";
 import Todo from "../components/Todo.js";
 import FormValidator from "../components/FormValidator.js";
 import { v4 as uuidv4 } from "https://jspm.dev/uuid";
-
+ 
 const addTodoButton = document.querySelector(".button_action_add");
 const addTodoPopup = document.querySelector("#add-todo-popup");
 const addTodoForm = addTodoPopup.querySelector(".popup__form");
 const addTodoCloseBtn = addTodoPopup.querySelector(".popup__close");
 const todosList = document.querySelector(".todos__list");
-
+ 
 const openModal = (modal) => {
   modal.classList.add("popup_visible");
 };
-
+ 
 const closeModal = (modal) => {
   modal.classList.remove("popup_visible");
 };
-
+ 
 const formValidator = new FormValidator(validationConfig, addTodoForm);
 formValidator.enableValidation();
-
+ 
 function renderTodo(item) {
   const todo = new Todo(item, "#todo-template");
   const todoElement = todo.getView();
   todosList.append(todoElement);
 }
-
+ 
 addTodoButton.addEventListener("click", () => {
   openModal(addTodoPopup);
 });
-
+ 
 addTodoCloseBtn.addEventListener("click", () => {
   closeModal(addTodoPopup);
 });
-
+ 
 addTodoForm.addEventListener("submit", (evt) => {
   evt.preventDefault();
-
+ 
   const name = evt.target.name.value;
   const dateInput = evt.target.date.value;
-
+ 
   const date = new Date(dateInput);
   date.setMinutes(date.getMinutes() + date.getTimezoneOffset());
-
+ 
   renderTodo({
     id: uuidv4(),
     name,
     date,
     completed: false,
   });
-
+ 
   formValidator.resetValidation();
   closeModal(addTodoPopup);
 });
-
+ 
 initialTodos.forEach(renderTodo);
